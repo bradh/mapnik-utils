d2c138cde3edc384bc560f21a2cd64f92f0c781a
robert.coup <robert.coup@dbea3d40-3153-0410-b551-57805770c6e6>
2010-07-27 20:49:52 +0000

    Allow [zoom] filters to work in cascasdenik-compile

--- branches/cascadenik-xmlbad/cascadenik-compile.py
+++ branches/cascadenik-xmlbad/cascadenik-compile.py
@@ -13,6 +13,8 @@ def main(file, dir, move_local_files):
         to the named directory.
     """
     mmap = mapnik.Map(1, 1)
+    # allow [zoom] filters to work
+    mmap.srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null'
     cascadenik.load_map(mmap, file, dir, move_local_files)
     
     (handle, filename) = tempfile.mkstemp(suffix='.xml', prefix='cascadenik-mapnik-')
