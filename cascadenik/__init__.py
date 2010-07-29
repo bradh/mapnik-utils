2c5905840ddd0eb30e6f89a07f8ba8c165b8998c
robert.coup <robert.coup@dbea3d40-3153-0410-b551-57805770c6e6>
2010-07-29 00:40:39 +0000

    touch up __init__.py adding doc string, skipping mapnik2 support for now, and setting up for ability to pass kwargs to compile
    Conflicts:
    
    	serverside/cascadenik/cascadenik/__init__.py

--- cascadenik/__init__.py
+++ cascadenik/__init__.py
@@ -1,9 +1,22 @@
+""" cascadnik - cascading sheets of style for mapnik
+
+http://mike.teczno.com/notes/cascadenik.html
+
+http://code.google.com/p/mapnik-utils/wiki/Cascadenik
+
+"""
+
 import style
 # compile module
 import compile as _compile
 # compile function
 from compile import compile
 from style import stylesheet_declarations
+
+# define Cascadenik version
+VERSION = '0.2.0'
+
+__all__ = ['compile','_compile','style','stylesheet_declarations']
 
 def load_map(map, input, dir=None, move_local_files=False):
     """
