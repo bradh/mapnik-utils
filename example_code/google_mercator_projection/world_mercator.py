c29f282e941f069c85d963109c8c06d098c8b6e4
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2010-09-15 15:08:10 +0000

    fixup code comments

--- example_code/google_mercator_projection/world_mercator.py
+++ example_code/google_mercator_projection/world_mercator.py
@@ -5,12 +5,15 @@ mapfile = 'mapfile.xml'
 map_output = 'map/spherical_mercator.png'
 m = Map(600, 400)
 load_map(m, mapfile)
+
+# http://spatialreference.org/ref/sr-org/6/ (aka epsg:900913)
+# also same as the new epsg:3857 (not yet at sr.org)
 spherical_mercator = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over'
 
-# Override projection in mapfile, setting rendered map projection
+# ensure the target map projection is spherical mercator
 m.srs = spherical_mercator
 
-# Set a bbox by reprojection Long/Lat coordinates
+# Set a bbox by reprojecting Long/Lat coordinates to spherical mercator
 p = Projection(spherical_mercator)
 merc = p.forward(Envelope(-180.0,-45.0,180.0,80.0))
 
