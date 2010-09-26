454781d08871592fcf2136c8c86bde1ce80cfe41
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2010-09-26 23:49:03 +0000

    recursively register fonts as this should have been doing all along

--- nik2img/mapnik_utils/fonts.py
+++ nik2img/mapnik_utils/fonts.py
@@ -1,7 +1,9 @@
+import os
 from mapnik_utils.version_adapter import Mapnik
 
 mapnik = Mapnik()
 
+valid_extensions = ['.ttf','.otf','.ttc','.pfa','.pfb','.ttc','.dfont']
 
 class FontHandler(object):
     def __init__(self):
@@ -16,7 +18,19 @@ class FontHandler(object):
     def add_fonts(self,fonts):
         engine = mapnik.FontEngine.instance()
         for font in fonts:
-            if engine.register_font(font):
-                self.added.append(font)
+            if os.path.isdir(font):
+                found = False;
+                for dirpath, _, filenames in os.walk(font):
+                    for filename in filenames:
+                        if os.path.splitext(filename)[1] in valid_extensions:
+                            font_name = os.path.join(dirpath, filename)
+                            if engine.register_font(font_name):
+                                self.added.append(font_name)
+                            else:
+                                self.failed.append(font_name)
             else:
-                self.failed.append(font)
+                if engine.register_font(font):
+                    self.added.append(font)
+                else:
+                    self.failed.append(font)
+
