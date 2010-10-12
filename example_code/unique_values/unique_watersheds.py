263bf81e4addcacb8134f6bfb5ae2ce57b749d18
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2010-10-12 15:34:12 +0000

    clean up example and use ability in recent mapnik versions to query for features and set the gamma

--- example_code/unique_values/unique_watersheds.py
+++ example_code/unique_values/unique_watersheds.py
@@ -1,66 +1,55 @@
 #!/usr/bin/env python
 
-from mapnik import *
-from random import *
+import mapnik
+import random
 
-UNIQUE = True
-
-m = Map(1000,550)
-m.background = Color('transparent')
-
-# http://spatialreference.org/ref/user/north-pacific-albers-conic-equal-area/
-north_pacific_albers = '+proj=aea +lat_1=30 +lat_2=70 +lat_0=52 +lon_0=-170 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
-m.srs = north_pacific_albers
+if not mapnik.mapnik_version() > 700:
+    raise SystemExit('At least Mapnik 0.7.0 is required for this script')
 
 def random_rgb():
-  return int(random() * 255)
+  return int(random.random() * 255)
 
 def generate_unique_values(features):
-  s = Style()
-  if UNIQUE:
-      for feat in range(1,features+1):
-        r=Rule('%s' % feat)
+    s = mapnik.Style()
+    for feat in range(1,features+1):
+        r = mapnik.Rule('%s' % feat)
         condition = '[ID]=%s' % (feat)
-        r.filter = Filter(condition)
+        r.filter = mapnik.Filter(condition)
         rgb = '%s%s,%s%s,%s%s' % (random_rgb(),'%',random_rgb(),'%',random_rgb(),'%')
-        r.symbols.append(PolygonSymbolizer(Color('rgb(%s)' % rgb)))
-        r.symbols.append(LineSymbolizer(Color('black'),.1))
+        p = mapnik.PolygonSymbolizer(mapnik.Color('rgb(%s)' % rgb))
+        p.gamma = .65
+        r.symbols.append(p)
+        #r.symbols.append(LineSymbolizer(Color('black'),.1))
         s.rules.append(r)
-  else:
-      r=Rule()
-      r.symbols.append(PolygonSymbolizer(Color('steelblue')))
-      r.symbols.append(LineSymbolizer(Color('black'),.1))
-      s.rules.append(r)  
-  return s
+    return s
 
-# Fetch feature count from ogrinfo
-# $ ogrinfo -so -al ../../../data/north_pacific_ecoregions.shp | grep 'Feature Count:'
-# Feature Count: 37
-features = 37 # Mapnik will need to expose datasource properties to be able to read this dynamically
+if __name__ == '__main__':
 
-# Attach random RGB values to PolygonSymbolizer rule and return the style
-unique_style = generate_unique_values(features)
+    # http://spatialreference.org/ref/user/north-pacific-albers-conic-equal-area/
+    north_pacific_albers = '+proj=aea +lat_1=30 +lat_2=70 +lat_0=52 +lon_0=-170 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
 
-m.append_style('Random RGB values',unique_style)
+    m = mapnik.Map(1000, 550, north_pacific_albers)
+    
+    lyr = mapnik.Layer('shape',north_pacific_albers)
+    lyr.datasource = mapnik.Shapefile(file='../../data/north_pacific_ecoregions.shp')
+    num_features = len(lyr.datasource.all_features())
+    
+    # Attach random RGB values to PolygonSymbolizer
+    m.append_style('Random RGB values',generate_unique_values(num_features))
+    lyr.styles.append('Random RGB values')
+    
+    # text styles, placed in separate style so text is always on top
+    s,r = mapnik.Style(),mapnik.Rule()
+    t = mapnik.TextSymbolizer('ECOREGION', 'DejaVu Sans Bold', 10, mapnik.Color('black'))
+    t.wrap_width = 40
+    t.halo_fill = mapnik.Color('white')
+    t.halo_radius = 1
+    r.symbols.append(t)
+    s.rules.append(r)  
+    m.append_style('Labels',s)
+    lyr.styles.append('Labels')
 
-# place in separate style, so that the text is always on top
-s,r = Style(),Rule()
-t = TextSymbolizer('ECOREGION', 'DejaVu Sans Bold', 10, Color('black'))
-t.halo_fill = Color('white')
-t.halo_radius = 1
-r.symbols.append(t)
-s.rules.append(r)  
-m.append_style('Labels',s)
-
-lyr = Layer('shape')
-lyr.datasource = Shapefile(file='../../data/north_pacific_ecoregions')
-lyr.srs = north_pacific_albers
-lyr.styles.append('Random RGB values')
-lyr.styles.append('Labels')
-m.layers.append(lyr)
-m.zoom_to_box(lyr.envelope())
-
-if UNIQUE:
-    render_to_file(m,'map/unique_values.png')
-else:
-    render_to_file(m,'map/single_value.png')
+    m.layers.append(lyr)
+    m.zoom_to_box(lyr.envelope())
+    
+    mapnik.render_to_file(m,'map/unique_values.png')
